438a1613effa3e749a9a2a0e063a07a30cc160bd
Chad Harper <chadharper811@gmail.com>
2025-10-17 07:39:52 -0500

    added single empoyee data to index file

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 import chalk from 'chalk';
 
-console.log(`${chalk.blue.bold('First Name: ')} David`);
-console.log(`${chalk.green.bold('Last Name: ')} Tucker`);
+// console.log(`${chalk.blue.bold('First Name: ')} David`);
+// console.log(`${chalk.green.bold('Last Name: ')} Tucker`);
+
+const employee = {
+    id: 0,
+    email: "Lauren_Shaffer@globomantics.com",
+    firstName: "Lauren",
+    lastName: "Shaffer",
+    salaryUSD: 246463,
+    loacalCurreny: "PEN",
+    dateBirth: "1988-02-08",
+    isActive: true
+};
